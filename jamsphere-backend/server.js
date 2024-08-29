a17ea6136ffc6f0c4dc8bc4655b04bc779f317e8
Kirubel Alemu <kirubelalemu119@gmail.com>
2024-08-29 13:47:26 +0300

    PATCH method added
    put used for adding, patch for updating.

--- jamsphere-backend/server.js
+++ jamsphere-backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(
     cors({
       origin: "*",
-      methods: ["GET", "POST", "PUT", "DELETE"],
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       credentials: true,
     }),
   );
